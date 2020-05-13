15d232b62f0b4c5db62b9623897faa3b07a8911e
Mikael Barnholdt <mikael.barnholdt@hotmail.com>
2020-05-13 10:06:10 +0200

    Cleanup usings

--- WebApi/Areas/Api/PatientsController.cs
+++ WebApi/Areas/Api/PatientsController.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebAPI.Data.Entities;
 using WebApi.Data;
-using System.Text.Json;
-using System.Net.Http;
-using System.Text;
-using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
 
 namespace WebApi.Areas.Api
